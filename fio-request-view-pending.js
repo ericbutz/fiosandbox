bde6d5619902862b5c8fcdd9300290a8674962e5
Eric Butz <ericgbutz@gmail.com>
2021-02-04 08:34:10 -0700

    Add get pending script

---
+++ fio-request-view-pending.js
@@ -0,0 +1,50 @@
+const {FIOSDK } = require('@fioprotocol/fiosdk')
+fetch = require('node-fetch')
+
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+//const baseUrl = 'https://testnet.fioprotocol.io:443/v1/'
+const baseUrl = 'https://fio.greymass.com/v1/'
+
+let privateKey = '',
+  publicKey = ''
+
+
+const getPendingFioRequests = async () => {
+  let result
+
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    baseUrl,
+    fetchJson
+  )
+
+  try {
+    result = await user.genericAction('getPendingFioRequests', {
+      limit: '',
+      offset: ''
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+  }
+
+  for (request in result.requests) {
+    console.log('\nRequest #', request);
+    console.log('payer_fio_address: ', result.requests[request].payer_fio_address);
+    console.log('payee_fio_address: ', result.requests[request].payee_fio_address);
+    console.log('payer_fio_public_key: ', result.requests[request].payer_fio_public_key);
+    console.log('payee_fio_public_key: ', result.requests[request].payee_fio_public_key);
+    console.log('Content payee_public_address: ', result.requests[request].content.payee_public_address);
+    console.log('Content amount: ', result.requests[request].content.amount);
+    console.log('Content chain_code: ', result.requests[request].content.chain_code);
+    console.log('Content token_code: ', result.requests[request].content.token_code);
+    console.log('Content memo: ', result.requests[request].content.memo);
+  }
+  
+}
+
+getPendingFioRequests();
