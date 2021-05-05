f2414c530e8697bec9cebc3106e88cab390b680b
Eric Butz <ericgbutz@gmail.com>
2021-05-05 14:37:09 -0600

    fix

--- fio-unwraptokens.js
+++ fio-unwraptokens.js
@@ -7,13 +7,13 @@ const fetchJson = async (uri, opts = {}) => {
   return fetch(uri, opts)
 }
 
-const baseUrl = 'http://44.242.158.42:8889/v1/'
+const baseUrl = ''
 
-let privateKey = '5KQ6f9ZgUtagD3LZ4wcMKhhvK9qy4BuwL3L1pkm6E2v62HCne2R',
-  publicKey = 'FIO7jVQXMNLzSncm7kxwg9gk7XUBYQeJPk8b6QfaK5NVNkh3QZrRr',
+let privateKey = '',
+  publicKey = '',
   amount = 50000000000,
-  obtId = '0x64f3350e4516a2c8509849b36ab680f7ad40811c1923d8979c4292f6883919d2',
-  fioAddress = 'bp1@Dapixdev'
+  obtId = '',
+  fioAddress = ''
 
 
 const fioUnwrap = async () => {
@@ -32,8 +32,8 @@ const fioUnwrap = async () => {
       data: {
         fio_address: fioAddress,
         amount: amount,
-        obt_id: '0x64f3350e4516a2c8509849b36ab680f7ad40811c1923d8979c4292f6883919d2',
-        actor: "qbxn5zhw2ypw"
+        obt_id: obtId,
+        actor: ''
       }
     })
     console.log('Result: ', result)
